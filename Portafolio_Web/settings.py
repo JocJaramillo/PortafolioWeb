df0463b8b568e529e8e24cf58ecc4c99ddd02093
JocJaramillo <cristhian10jaramillo@gmail.com>
2025-12-11 15:07:01 -0500

    Se sigue mejorando
    mejorando

--- Portafolio_Web/settings.py
+++ Portafolio_Web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-maojc*3_#g1mnote%ftx%(kq2i9_3nku=a0k$rpykphp+ja9t!'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-maojc*3_#g1mnote%ftx%(kq2i9_3nku=a0k$rpykphp+ja9t!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
